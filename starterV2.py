84f9ad9734f7e005ce33574f5ea5251828ec0bb5
Ewerton Douglas <Tec.-.Ewerton@hotmail.com>
2024-12-25 04:54:46 -0300

    Adicionando a alteração de salvar os dados num BD

--- starterV2.py
+++ starterV2.py
@@ -3,6 +3,23 @@ from datetime import datetime
 import os
 import win32print
 import win32api
+from tkcalendar import DateEntry
+from tkinter.ttk import Treeview
+import sqlite3
+
+# Conectar Banco de Dados
+banco = sqlite3.connect("Clientes_Pet.db")
+Cursor = banco.cursor()
+BD_COMMAND = "SELECT * FROM Clientes_Pet WHERE Data = ?"
+
+banco.execute('''CREATE TABLE IF NOT EXISTS Clientes_PET(
+                  Data TEXT,
+                  Posicao TEXT,
+                  Dono TEXT,
+                  Número TEXT,
+                  Pet TEXT,
+                  Procedimento TEXT
+                  )''')
 
 
 def imprimir(QTD):
@@ -13,7 +30,7 @@ def imprimir(QTD):
     diretorio = "C:/Users/waldo/ComandasPet/Comandas"
     lista_arquivos = os.listdir(diretorio)
 
-    for i in range(0,QTD,1):
+    for i in range(0, QTD, 1):
         for arquivo in lista_arquivos:
             win32api.ShellExecute(0, "print", arquivo, None, diretorio, 0)
 
@@ -43,6 +60,12 @@ ____________[ {data_ban["date"]} ]___________
 =====================================    
     """
 
+    Cursor.execute(f"""INSERT INTO Clientes_PET VALUES('{datetime.now().strftime("%Y-%m-%d")}',
+                    '{data_ban["position"]}', '{data_ban["client"]}', '{data_ban["number"]}',
+                '{data_ban["pet"]}', '{data_ban["process"]}') """)
+    banco.commit()
+
+
     try:
         os.makedirs("Comandas")
 
@@ -65,18 +88,19 @@ ____________[ {data_ban["date"]} ]___________
 
 
 def ImpresEntrega():
-
     try:
-        data_entr = {"client": clientdelivery_entry.get(), "number": numberdelivery_entry.get(), "adress": adress_entry.get(),
-                 "items": items_entry.get(), "value": round(int(value_entry.get()),2), "payment": pag_var.get(),
-                 "obs": obsdelivery_entry.get()}
+        data_entr = {"client": clientdelivery_entry.get(), "number": numberdelivery_entry.get(),
+                     "adress": adress_entry.get(),
+                     "items": items_entry.get(), "value": round(int(value_entry.get()), 2), "payment": pag_var.get(),
+                     "obs": obsdelivery_entry.get()}
 
     except:
-        data_entr = {"client": clientdelivery_entry.get(), "number": numberdelivery_entry.get(), "adress": adress_entry.get(),
-                 "items": items_entry.get(), "value": value_entry.get(), "payment": pag_var.get(),
-                 "obs": obsdelivery_entry.get()}    
+        data_entr = {"client": clientdelivery_entry.get(), "number": numberdelivery_entry.get(),
+                     "adress": adress_entry.get(),
+                     "items": items_entry.get(), "value": value_entry.get(), "payment": pag_var.get(),
+                     "obs": obsdelivery_entry.get()}
 
-    text_imp_ent =f"""
+    text_imp_ent = f"""
      Pet's Food - Graciliano
 =====================================
                 Dados
@@ -118,36 +142,49 @@ def ImpresEntrega():
             adress_entry.delete(0, 50)
             items_entry.delete(0, 500)
             value_entry.delete(0, 50)
-            obsdelivery_entry.delete(0,100)
+            obsdelivery_entry.delete(0, 100)
 
 
-### Definindo Configurações e Alguns Detalhes do Aplicativo ###
+def datefilter():
+
+    for item in pets_table.get_children():
+        pets_table.delete(item)
+
+    date_today = date_filter_entry.get_date()
+    for data in Cursor.execute(BD_COMMAND, (date_today,)):
+        pets_table.delete()
+        real_data = [data[1], data[2], data[3], data[4], data[5], ]
+        pets_table.insert(parent="", index=0, values=real_data)
+
+
+# Definindo Configurações e Alguns Detalhes do Aplicativo
 
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
 app = ctk.CTk()
-app.geometry("350x515")
+app.geometry("370x515")
 app.resizable(False, False)
 app.title(f"Pets Food  |  {datetime.now().strftime("%d/%m/%Y")}")
 app.iconbitmap("icon.ico")
 
-### Fim de Bloco ###
+# Fim de Bloco
 
-### Base da Tela, onde vai ficar nosso TabView e titulo ###
+# Base da Tela, onde vai ficar nosso TabView e titulo
 
 title = ctk.CTkLabel(app, text="Pets Food", font=("Times", 36, "bold"), width=350, height=35)
 title.grid(row=1, column=1)
 
-tabview = ctk.CTkTabview(app, width=330, height=400, corner_radius=15, border_width=2
+tabview = ctk.CTkTabview(app, width=365, height=400, corner_radius=15, border_width=2
                          , fg_color="#c4c2c2", segmented_button_fg_color="grey", )
-tabview.grid(row=2, column=1)
+tabview.grid(row=2, column=1, padx=2)
 tabview.add("Pet Shop")
+tabview.add("Diario")
 tabview.add("Entregas")
 
-### Fim de Bloco ###
+# Fim de Bloco
 
-### Tela do banho e Tosa ###
+# Tela do banho e Tosa
 
 position_list = [
     '1º', '2º', '3º', '4º', '5º', '6º', '7º', '8º', '9º', '10º',
@@ -200,7 +237,7 @@ procediment_entry = ctk.CTkComboBox(tabview.tab("Pet Shop"), width=150, fg_color
 procediment_entry.grid(row=4, column=1, pady=3)
 
 obs_text = ctk.CTkLabel(tabview.tab("Pet Shop"), width=270, font=("Times", 23, "bold"), text="Observações Gerais:")
-obs_text.grid(row=5, column=0, pady=7, columnspan=2)
+obs_text.grid(row=5, column=0, pady=7, columnspan=2, padx=25)
 obs_entry = ctk.CTkTextbox(tabview.tab("Pet Shop"), width=290, height=55, fg_color="white", border_color="black",
                            border_width=2, corner_radius=8, font=("Times", 16))
 obs_entry.grid(row=6, column=0, pady=3, columnspan=2)
@@ -210,9 +247,9 @@ submit_buttom = ctk.CTkButton(tabview.tab("Pet Shop"), width=290, corner_radius=
                               text="Imprimir Comanda", command=ImpresBanho)
 submit_buttom.grid(row=7, column=0, columnspan=2, pady=7)
 
-### Fim do Bloco ###
+# Fim do Bloco
 
-### Tela Entregas ###
+# Tela Entregas
 
 clientdelivery_text = ctk.CTkLabel(tabview.tab("Entregas"), width=120, font=("Times", 23, "bold"), text="Cliente:")
 clientdelivery_text.grid(row=0, column=0, pady=3)
@@ -257,10 +294,10 @@ Pix_radio.grid(row=7, column=0, pady=8, columnspan=2)
 din_radio = ctk.CTkRadioButton(tabview.tab("Entregas"), text="Cartão", variable=pag_var, value="Cartão", width=50)
 din_radio.grid(row=7, column=1, pady=8)
 
-obsdelivery_text = ctk.CTkLabel(tabview.tab("Entregas"),width=120, font=("Times", 23, "bold"), text="Observações:")
+obsdelivery_text = ctk.CTkLabel(tabview.tab("Entregas"), width=120, font=("Times", 23, "bold"), text="Observações:")
 obsdelivery_text.grid(row=8, column=0, columnspan=2)
 obsdelivery_entry = ctk.CTkEntry(tabview.tab("Entregas"), width=290, placeholder_text="Tem alguma informação a mais?",
-                                 fg_color="white", border_color="black",border_width=2, corner_radius=8,
+                                 fg_color="white", border_color="black", border_width=2, corner_radius=8,
                                  font=("Times", 14, "bold"))
 obsdelivery_entry.grid(row=9, column=0, columnspan=2)
 
@@ -268,5 +305,39 @@ submitdelivery_buttom = ctk.CTkButton(tabview.tab("Entregas"), width=290, corner
                                       font=("Times", 24, "bold"),
                                       text="Realizar Entrega", command=ImpresEntrega)
 submitdelivery_buttom.grid(row=10, column=0, columnspan=2, pady=15)
+
+# Tela Pets Diarios
+
+date_filter_entry = DateEntry(tabview.tab("Diario"), date_pattern="d-m-y", cursor="hand2", fg_color="white",
+                              border_color="black", border_width=2, corner_radius=8, font=("Times", 13, "bold"), )
+date_filter_entry.grid(row=0, column=0, padx=4, pady=6)
+
+date_filter_submit = ctk.CTkButton(tabview.tab("Diario"), width=120, corner_radius=10, border_width=2,
+                                   font=("Times", 17, "bold"), text="Filtrar por data", command=datefilter)
+date_filter_submit.grid(row=0, column=1, padx=10, pady=6)
+
+pets_table = Treeview(tabview.tab("Diario"), columns=["Posição", "Dono", "Telefone", "Pet", "Procedimento"],
+                      show="headings",)
+pets_table.grid(row=1, column=0, columnspan=2, pady=4)
+
+pets_table.column("Posição", width=20)
+pets_table.heading("Posição", text="nº")
+
+pets_table.column("Dono", width=60)
+pets_table.heading("Dono", text="Dono")
+
+pets_table.column("Telefone", width=75)
+pets_table.heading("Telefone", text="Celular")
+
+pets_table.column("Pet", width=70)
+pets_table.heading("Pet", text="Pet")
+
+pets_table.column("Procedimento", width=110)
+pets_table.heading("Procedimento", text="Procedimento")
+
+date_today = datetime.now().strftime("%Y-%m-%d")
+for data in Cursor.execute(BD_COMMAND, (date_today,)):
+    real_data = [data[1], data[2], data[3], data[4], data[5],]
+    pets_table.insert(parent="", index=0, values=real_data)
 
 app.mainloop()
